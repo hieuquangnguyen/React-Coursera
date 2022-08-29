31dc2eff2fcdb31da6de3cc41ad5dcc6c9ba25ad
STS <hieunguyen.13092000@gmail.com>
2022-08-30 00:04:52 +0700

    Update week 2 - ex 1

--- src/App.js
+++ src/App.js
@@ -1,33 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+
 import { Component } from 'react';
-import { Navbar, NavbarBrand } from 'reactstrap';
-import MenuDetail from './components/DishDetailComponent';
 
-import { DISHES } from './shared/dishes';
-
+import Main from './components/MainComponent';
 class App extends Component  {
-
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      dishes: DISHES
-    };
-  }
 
   render (){
     return (
+      // <div className="App">
+        
+      //   {/* app.js là chính nề,  */}
+      //   {/* hiển thị Navbar*/}
+      //   <Navbar dark color="primary">
+      //       <div className="container">
+      //         <NavbarBrand href="/">Coursera</NavbarBrand>
+      //       </div>
+      //   </Navbar>
+      //   {/* Hiển thị Menu */}
+      //   <Menu dishes={this.state.dishes} />
+        
+      // </div>
       <div className="App">
-        {/* app.js là chính nề,  */}
-        {/* hiển thị Navbar*/}
-        <Navbar dark color="primary">
-            <div className="container">
-              <NavbarBrand href="/">Coursera</NavbarBrand>
-            </div>
-        </Navbar>
-        {/* Hiển thị Menu */}
-        <MenuDetail dishes={this.state.dishes} />
+         <Main />
       </div>
     );
   }
--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,52 +1,55 @@
 import React, { Component } from 'react';
-import { Media, NavItem } from 'reactstrap';
+import DishDetail from './DishDetailComponent';
 import '../App.css';
 // mỗi lần muốn dùng bootstrap gì thì pải import vào và from reactstrap
 import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap';
-import { DISHES } from '../shared/dishes';
-
+// file menu ni có thể hiểu là  Presentational Component
 class Menu extends Component {
 
  
   // khai bao constructor 
-  constructor(props) {
+  constructor(props){
     super(props);
-    // tao state, la tao ra cac mang du lieu, cac component
-
-    // state khác với dữ liệu thường
-    // state có thể render lại dữ liệu ra màn hình lại
-    // còn dữ liệu thường ko có cập nhập lại dữ liệu thay đổi ra màn hình
-    this.state = {
-      // gán ban đầu bằng null kiểu như chưa click cái nào hết
-      selectedDish: null,
-    };
   }
+  // constructor(props) {
+  //   super(props);
+  //   // tao state, la tao ra cac mang du lieu, cac component
+
+  //   // state khác với dữ liệu thường
+  //   // state có thể render lại dữ liệu ra màn hình lại
+  //   // còn dữ liệu thường ko có cập nhập lại dữ liệu thay đổi ra màn hình
+  //   this.state = {
+  //     // gán ban đầu bằng null kiểu như chưa click cái nào hết
+  //     selectedDish: null,
+      
+  //   };
+  // } --> đã chuyển sang Main
 
   // function onDishSelect
   // hàm khởi tạo có đối số
-  onDishSelect(dish) {
-    // setState để khowir tao gawns SelectedDish = dish
-    this.setState({ selectedDish: dish });
-  }
+  // onDishSelect(dish) {
+  //   // setState để khowir tao gawns SelectedDish = dish
+  //   this.setState({ selectedDish: dish });
+  // } --> đã được chuyển sang Main
 
   // renderDish khi click thì sẽ xuất hiện cái gì
   // render là chứa UI, cái mà sẽ hiển thị ra
-  renderDish(dish) {
-    if (dish != null)
-      return (
-        <Card>
-          <CardImg top src={dish.image} alt={dish.name} />
-          <CardBody>
-            <CardTitle className='dishName' >{dish.name}</CardTitle>
-            <CardText>{dish.description}</CardText>
-          </CardBody>
-        </Card>
-      );
-    else
-      return (
-        <div></div>
-      );
-  }
+  // renderDish(dish) {
+  //   if (dish != null)
+  //     return (
+  //       <Card>
+  //         <CardImg top src={dish.image} alt={dish.name} />
+  //         <CardBody>
+  //           <CardTitle className='dishName' >{dish.name}</CardTitle>
+  //           <CardText>{dish.description}</CardText>
+  //         </CardBody>
+  //       </Card>
+  //     );
+  //   else
+  //     return (
+  //       <div></div>
+  //     );
+  // }
 
   // renderComment(dish){
   //   if(dish!=null)
@@ -74,32 +77,18 @@ class Menu extends Component {
     const menu = this.props.dishes.map((dish) => {
       // return một card
       return (
-        <div className="col-12 col-md-5 m-1">
-          <Card key={dish.id}
-            // khi onclick vào một dish nào đó
-            onClick={() => this.onDishSelect(dish)} >
+        <div key={dish.id} className="col-12 col-md-5 m-1">
+          <Card // khi onclick vào một dish nào đó
+            onClick={() => this.props.onClick(dish.id)} > 
 
             <CardImg width="100%" src={dish.image} alt={dish.name} />
             <CardImgOverlay>
               <CardTitle>{dish.name}</CardTitle>
             </CardImgOverlay>
-            {/* <p>{dish.comments.map((cmt) => {
-              return (
-                <div>
-                  {cmt.author}
-                </div>
-              );
-            })}</p> */}
           </Card>
-
-          {/* gọi hàm rendercomment */}
-          {/* {this.renderComment(dish)} */}
-          
         </div>
       );// end return menu
     });
-
-    // return của render
     // return của render mới xuất hiện ra màn hình
     // return all
     return (
@@ -107,24 +96,9 @@ class Menu extends Component {
         <div className="row">
           {/* gọi biến menu hiển thị */}
           {menu}
-
-          {/* {this.state.test} */}
-          {/* {this.test} */}
-
         </div>
-        <div className="row">
-          <div className="col-12 col-md-5 m-1">
-            {/* gọi renderDish để hiển thị */}
-            {this.renderDish(this.state.selectedDish)}
-          </div>
-          <div className="col-12 col-md-5 m-1">
-          {/* dung toan tu cho ko dung cu phap dc */}
-          {/* {this.state.selectedDish != null ? <h4>comments</h4> : <div> </div>}
-          
-          {this.renderComment(this.state.selectedDish)} */}
-          {/* {this.renderComment(this.normaldish)} */}
-          </div>
-        </div>
+        {/* <DishDetail dish = {this.state.selectedDish}/> */}
+        {/* {DishDetail(this.state.selectedDish)} */}
       </div>
     );
   } // end return render
